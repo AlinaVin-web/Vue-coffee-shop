8778459e1e258a9956ab3824245523b69eb224fb
Alina Vinogradova <ellafoss@yandex.ru>
2025-02-11 20:53:43 +0300

    Динамическое подключение роутов

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,11 @@
-import { createWebHistory, createRouter } from 'vue-router'
-
-import HeroView from "../views/HeroView.vue";
-import OurCoffeeView from "../views/OurCoffeeView.vue";
-import ForYourPleasureView from "../views/ForYourPleasureView.vue";
-import ContactUsView from "../views/ContactUsView.vue";
-import ThankYouView from "../views/ThankYouView.vue";
-//import ItemView from "../views/ItemView.vue";
+import { createWebHistory, createRouter } from 'vue-router';
 
 const routes = [
-    { path: '/', component: HeroView },
-    { path: '/our-coffee', component: OurCoffeeView },
-    { path: '/for-your-pleasure', component: ForYourPleasureView },
-    { path: '/contact-us', component: ContactUsView },
-    { path: '/thank-you', component: ThankYouView }
-    //{ path: '/item', component: ItemView }
+    { path: '/', component: () => import('../views/HeroView.vue') },
+    { path: '/our-coffee', component: () => import('../views/OurCoffeeView.vue') },
+    { path: '/for-your-pleasure', component: () => import('../views/ForYourPleasureView.vue') },
+    { path: '/contact-us', component: () => import('../views/ContactUsView.vue') },
+    { path: '/thank-you', component: () => import('../views/ThankYouView.vue') }
 ];
 
 const router = createRouter({
